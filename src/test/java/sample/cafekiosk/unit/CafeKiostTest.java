dfdd827b3998827eb45ac77c2e032f790fe4323d
tilsong <cnsong0229@gmail.com>
2023-04-30 17:31:09 +0900

    inflearn class: practical test | 2.1 code

---
+++ src/test/java/sample/cafekiosk/unit/CafeKiostTest.java
@@ -0,0 +1,24 @@
+package sample.cafekiosk.unit;
+
+import org.junit.jupiter.api.Test;
+import sample.cafekiosk.unit.beverage.Americno;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CafeKiostTest {
+
+/*
+    // 최종 단계에서 사람이 개입해야 함
+    // 무엇이 성공이고, 실패인지가 확실하지 않음.
+    // => 수동 테스트 -> 자동 테스트 전환 필요
+    @Test
+    void add() {
+        CafeKiost cafeKiost = new CafeKiost();
+        cafeKiost.add(new Americno());
+
+        System.out.println(">>>>> 담긴 음료 수: " + cafeKiost.getBeverages().size());
+        System.out.println(">>>>> 담긴 음료 : " + cafeKiost.getBeverages().get(0).getName());
+    }
+*/
+
+}
