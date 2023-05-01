de75d098d23e04a4c76b63e188358d0b52f89bc7
tilsong <cnsong0229@gmail.com>
2023-05-02 01:27:35 +0900

    inflearn class: practical test | ~5.6 code

---
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -0,0 +1,124 @@
+package sample.cafekiosk.spring.api.service.order;
+
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.orderProduct.OrderProduct;
+import sample.cafekiosk.spring.domain.orderProduct.OrderProductRepository;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@DataJpaTest
+@ActiveProfiles("test")
+@SpringBootTest
+class OrderServiceTest {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+
+    @Autowired
+    private OrderService orderService;
+
+    @AfterEach
+    void tearDown() {
+        // productRepository.deleteAll();
+        orderProductRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrder() {
+        // given
+        Product product1 = createProduct(ProductType.HANDMADE, "001", 1000);
+        Product product2 = createProduct(ProductType.HANDMADE, "002", 2000);
+        Product product3 = createProduct(ProductType.HANDMADE, "003", 3000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "003"))
+                .build();
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 4000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        Tuple.tuple("001", 1000),
+                        Tuple.tuple("003", 3000)
+                );
+    }
+
+    @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
+    @Test
+    void createOrderWithDuplicateProductNumbers() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(ProductType.HANDMADE, "001", 1000);
+        Product product2 = createProduct(ProductType.HANDMADE, "002", 2000);
+        Product product3 = createProduct(ProductType.HANDMADE, "003", 3000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001"))
+                .build();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 2000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        Tuple.tuple("001", 1000),
+                        Tuple.tuple("001", 1000)
+                );
+    }
+
+    /**
+     * 테스트 코드 간소화를 위한 테스트 용 헬퍼 메서드
+     */
+    private Product createProduct(ProductType type, String productNumber, int price) {
+        return Product.builder()
+                .type(type)
+                .productNumber(productNumber)
+                .price(price)
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .name("메뉴이름")
+                .build();
+    }
+}
--- src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -14,7 +14,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 // @SpringBootTest
 @ActiveProfiles("test")
 @DataJpaTest // SpringBootTest 보다 가벼움, jpa 관련 속성만 주입
-// but springboottest 선호 왜? -> 다음 섹션에서 이야기할 것.
 public class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
@@ -48,6 +47,45 @@ public class ProductRepositoryTest {
 
         // when
         List<Product> products = productRepository.findAllBySellingStatusIn(List.of(ProductSellingStatus.SELLING, ProductSellingStatus.HOLD));
+
+        // then
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus")
+                .containsExactlyInAnyOrder(
+                        Assertions.tuple("001", "아메리카노", ProductSellingStatus.SELLING),
+                        Assertions.tuple("002", "카페라떼", ProductSellingStatus.HOLD)
+                );
+    }
+
+    @DisplayName("상품 번호 리스트로 상품들을 조회한다.")
+    @Test
+    void findAllByProductNumberIn() {
+        // given
+        Product product1 = Product.builder().
+                productNumber("001")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+        Product product2 = Product.builder().
+                productNumber("002")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(ProductSellingStatus.HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+        Product product3 = Product.builder().
+                productNumber("003")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(ProductSellingStatus.STOP_SELLING)
+                .name("팥빙수")
+                .price(7000)
+                .build();
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> products = productRepository.findAllByProductNumberIn(List.of("001", "002"));
 
         // then
         assertThat(products).hasSize(2)
